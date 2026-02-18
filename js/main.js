8e6af55026071c95f1472d97e20fa06d4837a6e3
applecake975 <applecake975@gmail.com>
2026-02-18 20:11:03 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,60 @@
+function toggleMenu() {
+  const $navMenu = document.getElementById('nav__menu');
+  $navMenu.classList.toggle('show');
+}
+function init() {
+  const $navToggle = document.getElementById('nav_toggle');
+  $navToggle.addEventListener('click', function () {
+    toggleMenu();
+  });
+
+  const $navLinkList = document.querySelectorAll('.nav__link');
+  $navLinkList.forEach((el) => el.addEventListener('click', toggleMenu));
+}
+init();
+
+const options = {
+  threshold: 1,
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    const sectionId = entry.target.id;
+    if (entry.isIntersecting) {
+      document
+        .querySelector(`.nav__link[href*=${sectionId}]`)
+        .classList.add('active_link');
+
+      const $items = document.querySelectorAll(
+        `.nav__link:not([href*=${sectionId}])`,
+      );
+      $items.forEach((el) => el.classList.remove('active_link'));
+    }
+  });
+}, options);
+
+const $sectionList = document.querySelectorAll('.section');
+$sectionList.forEach((el) => observer.observe(el));
+// observer.observe($workSection);
+// observer.observe($workSection);
+// observer.observe($workSection);
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2000,
+  delay: 200,
+});
+
+scrollReveal.reveal('.home__data, .about__img,.skills__text');
+scrollReveal.reveal('.home__img,.about__data,.skills__img', { delay: 400 });
+scrollReveal.reveal('.skills__data,.work__link,.contact__input', {
+  interval: 200,
+});
+
+const $floatingButton = document.getElementById('floatingButton');
+$floatingButton.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    // behavior: 'smooth',
+  });
+});
